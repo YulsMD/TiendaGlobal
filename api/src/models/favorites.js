7f6f9d03eb6051ababf8b7025f13e6e8fe256f55
Nabil <nallis014@gmail.com>
2023-06-27 19:40:18 -0300

    add models review and favorites

---
+++ api/src/models/favorites.js
@@ -0,0 +1,21 @@
+import * as timestamps from 'mongoose-timestamp'
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    'favorites',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      idProduct: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  )
+}
